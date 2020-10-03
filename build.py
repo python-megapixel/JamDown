e9239447561292dd8141042d1fb1da717fce1d61
pythonmegapixel <34344190+python-megapixel@users.noreply.github.com>
2020-10-03 17:50:33 +0100

    remove spurious comma

--- build.py
+++ build.py
@@ -15,7 +15,7 @@ def style(passedParams):
 		head = "<style>" + template.read() + "</style>" + top
 		
 
-commands = {"useTemplate": useTemplate, "linkStyle": style,}
+commands = {"useTemplate": useTemplate, "linkStyle": style}
 
 def doCmd(cmd):
 	global commands
