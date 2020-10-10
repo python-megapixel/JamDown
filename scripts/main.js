d2de983fc5d8423bde10b5d41cd8e976bad8c516
pythonmegapixel <34344190+python-megapixel@users.noreply.github.com>
2020-10-10 10:11:30 +0100

    Use updated (fixed) main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,16 @@
 function showMenu() {
-			document.getElementById("side-menu").style = "min-height: 50%;width:50%position: fixed;z-index: 1;top: 0;left: 0;background-color:#2196F3;overflow-x: hidden;padding-top: 20px;";
+			document.getElementById("side-menu").style = "min-height: 50%;width:50%position: fixed;z-index: 1;top: 0;left: 0;background-color:#FF0000;overflow-x: hidden;padding-top: 20px;";
 
 			var ch = document.getElementById("side-menu").children;
 			for (i=2; i<ch.length; i++) {
 				ch[i].style = "padding: 6px 8px 6px 16px;width: 100%;text-decoration: none;color: white;display: block;font-size:1em;"
 			}
 
-			document.getElementById("subt1").style.textDecoration = "underline";
-			document.getElementById("subt2").style.textDecoration = "underline";
+			// document.getElementById("subt1").style.textDecoration = "underline";
+			// document.getElementById("subt2").style.textDecoration = "underline";
 
-			document.getElementById("subt1").style.color = "black";
-			document.getElementById("subt2").style.color = "black";
+			// document.getElementById("subt1").style.color = "black";
+			// document.getElementById("subt2").style.color = "black";
 
 			document.getElementById("menu-button").style.display = "none";
 			document.getElementById("close-button").style.display = "block";
